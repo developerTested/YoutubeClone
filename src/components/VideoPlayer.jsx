42abd66aa6ab71a3dd0d17ed03116db66a572913
Developer Tested <89337605+developerTested@users.noreply.github.com>
2023-08-14 03:11:22 +0530

    Tooltip

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -2,6 +2,7 @@ import useVideoPlayer from '../utilities/useVideoPlayer';
 import moment from 'moment';
 import React, { useEffect, useRef, useState } from 'react'
 import { MdClosedCaption, MdFullscreen, MdFullscreenExit, MdPause, MdPlayArrow, MdRefresh, MdSettings, MdVolumeDown, MdVolumeOff, MdVolumeUp } from "react-icons/md";
+import Tooltip from './Tooltip';
 
 export default function VideoPlayer({ src = 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', ...props }) {
 
@@ -62,74 +63,94 @@ export default function VideoPlayer({ src = 'https://commondatastorage.googleapi
     };
 
     return (
-        <div className='relative group block m-auto w-full h-1/2 bg-gradient-to-t from-black/75'>
-            <video
-                id='player-video'
-                className='w-full h-full'
-                ref={videoRef}
-                onTimeUpdate={handleProgress}
-                {...props}
-            >
+        <>
+            <div className="mt-24">
+                <Tooltip position="top" title="Top Tooltip">
+                    Hello
+                </Tooltip>
 
-                <source src={src} type="video/mp4" />
-            </video>
-            <div className={`animation absolute -z-0 inset-0 w-full h-full flex items-center justify-center`}>
-                <button className={`${playerState.isPlaying ? 'animate-play-fade' : 'animate-pause-fade'} bg-black text-white rounded-full p-4`}>
-                    {playerState.isPlaying ? <MdPlayArrow className='w-8 h-8' /> : playerState.isEnded ? <MdRefresh className="w-8 h-8" /> : <MdPause className="w-8 h-8" />}
-                </button>
+                <Tooltip position="bottom" title="Bottom Tooltip">
+                    Hello
+                </Tooltip>
 
-                {playerState.isEnded &&
-                    <button onClick={() => videoRef.current.play()} className="bg-black text-white rounded-full p-4">
-                        <MdRefresh className="w-8 h-8" />
-                    </button>
-                }
+                <Tooltip position="left" className="Left Tooltip">
+                    Hello
+                </Tooltip>
+
+                <Tooltip position="right" className="Right Tooltip">
+                    Hello
+                </Tooltip>
             </div>
-            <div className="controls z-10 hidden group-hover:block hover:block w-full absolute left-0 right-0 bottom-0 hover:z-10 text-white bg-gradient-to-t from-black/75">
-                <div className="player-progress bg-white/20 w-full h-1"></div>
-                <input
-                    className='w-full'
-                    type="range"
-                    min="0"
-                    max="100"
-                    value={playerState.progress}
-                    onChange={(e) => handleVideoProgress(e)}
-                />
-                <div className="controls flex items-center justify-between gap-2 p-2">
-                    <div className="flex items-center gap-2">
 
-                        <button onClick={togglePlay}>
-                            {playerState.isPlaying ? <MdPause className='w-8 h-8' /> : playerState.isEnded ? <MdRefresh className="w-8 h-8" /> : <MdPlayArrow className="w-8 h-8" />}
+            <div className='relative group block m-auto w-full h-1/2 bg-gradient-to-t from-black/75'>
+                <video
+                    id='player-video'
+                    className='w-full h-full'
+                    ref={videoRef}
+                    onTimeUpdate={handleProgress}
+                    {...props}
+                >
+
+                    <source src={src} type="video/mp4" />
+                </video>
+                <div className={`animation absolute -z-0 inset-0 w-full h-full flex items-center justify-center`}>
+                    <button className={`${playerState.isPlaying ? 'animate-play-fade' : 'animate-pause-fade'} bg-black text-white rounded-full p-4`}>
+                        {playerState.isPlaying ? <MdPlayArrow className='w-8 h-8' /> : playerState.isEnded ? <MdRefresh className="w-8 h-8" /> : <MdPause className="w-8 h-8" />}
+                    </button>
+
+                    {playerState.isEnded &&
+                        <button onClick={() => videoRef.current.play()} className="bg-black text-white rounded-full p-4">
+                            <MdRefresh className="w-8 h-8" />
                         </button>
+                    }
+                </div>
+                <div className="controls z-10 hidden group-hover:block hover:block w-full absolute left-0 right-0 bottom-0 hover:z-10 text-white bg-gradient-to-t from-black/75">
+                    <div className="player-progress bg-white/20 w-full h-1"></div>
+                    <input
+                        className='w-full'
+                        type="range"
+                        min="0"
+                        max="100"
+                        value={playerState.progress}
+                        onChange={(e) => handleVideoProgress(e)}
+                    />
+                    <div className="controls flex items-center justify-between gap-2 p-2">
+                        <div className="flex items-center gap-2">
 
-                        <button className='flex items-center gap-2 group' onClick={toggleMute}>
-                            {playerState.isMuted ? (
-                                <MdVolumeOff className='w-6 h-6' />
-                            ) : (
-                                <MdVolumeUp className='w-6 h-6' />
-                            )}
+                            <button onClick={togglePlay}>
+                                {playerState.isPlaying ? <MdPause className='w-8 h-8' /> : playerState.isEnded ? <MdRefresh className="w-8 h-8" /> : <MdPlayArrow className="w-8 h-8" />}
+                            </button>
 
-                            <input
-                                className='hidden group-hover:block w-20'
-                                type="range"
-                                min="0"
-                                max="1"
-                                value={videoRef.current.volume}
-                                onChange={(e) => handleVideoProgress(e)}
-                            />
-                        </button>
-                        <div className="block currentTime">{formatDuration(videoRef.current?.currentTime)}</div>
-                        <div className="block">/</div>
-                        <div className="block">{formatDuration(videoRef.current?.duration)}</div>
-                    </div>
+                            <button className='flex items-center gap-2 group' onClick={toggleMute}>
+                                {playerState.isMuted ? (
+                                    <MdVolumeOff className='w-6 h-6' />
+                                ) : (
+                                    <MdVolumeUp className='w-6 h-6' />
+                                )}
 
-                    <div className="flex items-center gap-2">
-                        <MdClosedCaption className='w-6 h-6' />
-                        <MdSettings className='w-6 h-6' />
-                        <MdFullscreen className='w-6 h-6' />
-                        <MdFullscreenExit className='w-6 h-6' />
+                                <input
+                                    className='hidden group-hover:block w-20'
+                                    type="range"
+                                    min="0"
+                                    max="1"
+                                    value={videoRef.current.volume}
+                                    onChange={(e) => handleVideoProgress(e)}
+                                />
+                            </button>
+                            <div className="block currentTime">{formatDuration(videoRef.current?.currentTime)}</div>
+                            <div className="block">/</div>
+                            <div className="block">{formatDuration(videoRef.current?.duration)}</div>
+                        </div>
+
+                        <div className="flex items-center gap-2">
+                            <MdClosedCaption className='w-6 h-6' />
+                            <MdSettings className='w-6 h-6' />
+                            <MdFullscreen className='w-6 h-6' />
+                            <MdFullscreenExit className='w-6 h-6' />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
